7abda9ce9e520024e89be1faba03861867a7f254
Ahmed Henawey <henawey@gmail.com>
2018-04-25 09:55:31 +0200

    Initial Commit

---
+++ Weather/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Ahmed Ali Henawey on 25/04/2018.
+//  Copyright © 2018 Ahmed Ali Henawey. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
